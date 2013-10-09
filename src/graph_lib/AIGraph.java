c010610dd98673c220bf2bf28f3d5896ec6b827a
Flah <flah.ahmad@haw-hamburg.de>
2013-10-09 19:35:14 +0200

    Alle Konstruktoren Methoden Bodies und alle Selektoren Bodies erstellt. Es müssen noch ein paar Instanzvariablen hinzugefügt werden, fällt einem auf, wenn man sich die Methoden durch liest :)

--- src/graph_lib/AIGraph.java
+++ src/graph_lib/AIGraph.java
@@ -8,6 +8,8 @@ public class AIGraph {
 	private List<Vertex> verticesList = new ArrayList<>();
 	private List<Edge> edgesList = new ArrayList<>();
 	
+	//********************************* KONSTRUKTOREN *********************************************
+	
 	public AIGraph() {
 		//NULL Graph
 	}
@@ -19,24 +21,90 @@ public class AIGraph {
 	}
 	
 	public void deleteVertex(Vertex v_id) {
-		
+		//Löschen des Knotens
+		verticesList.remove(v_id);
+		//Alle Inzidenten Kanten löschen
+		for(Edge edge : edgesList) {
+			//Wenn eine der beiden Kantenseiten auf den Knoten, wird die Kante gelöscht
+			if((edge.verticesFromEdge[0] == v_id) || (edge.verticesFromEdge[1] == v_id)) {
+				edgesList.remove(edge);
+			}
+		}
 	}
+	
+	public Edge addEdgeU(Vertex v1, Vertex v2) {
+		// TODO: addEdgeU implementieren --> für ungerichtete Kanten
+		return null;
+	}
+	
+	public Edge addEdgeD(Vertex v1, Vertex v2) {
+		// TODO: addEdgeD implementieren --> für gerichtete Kanten
+		return null;
+	}
+	
+	public Edge deleteEdge(Vertex v1, Vertex v2) {
+		// TODO: deleteEdge --> für gerichtete und ungerichtete
+		return null;
+	}
+	
+	//********************************* SELEKTOREN *********************************************
+	
+	public boolean isEmpty() {
+		// TODO: isEmpty implementieren
+		return false;
+	}
+	
+	public Edge getSource(Edge e1) {
+		// TODO: getSource implementieren --> ermittelt im gerichteten Fall die Quelle der
+		// Kante e1 (gegeben als ID) im ungerichteten Fall die linke / erste Ecke
+		return null;
+	}
+	
+	public Edge getTarget(Edge e1) {
+		// TODO: getTarget implementieren --> ermittelt im gerichteten Fall die Senke der
+		// Kante e1 (gegeben als ID) im ungerichteten Fall die rechte / zweite Ecke
+		return null;
+	}
+	
+	public List<Edge> getIncident(Vertex v1) {
+		// TODO: getIncident implementieren --> ermittelt alle zur Ecke v1
+		// inzidenten Kanten
+		return null;
+	}
+	
+	public List<Edge> getAdjacent(Vertex v1) {
+		// TODO: getAdjacent implementieren --> ermittelt alle zur Ecke v1
+		// adjazenten Ecken
+		return null;
+	}
+	
+	public List<Vertex> getVertexes() {
+		// TODO: getVertexes implementieren --> ermittelt alle Ecken des Graphen
+		return null;
+	}
+	
+	public List<Edge> getEdges() {
+		// TODO: getEdges implementieren --> emittelt alle Kanten des Graphen
+		return null;
+	}
+	
+	//******************************** PRIVATE KLASSEN ****************************************
 	
 	private class Vertex {
 		
 		private int vertexValue;
-		private List<Edge> inzidenten = new ArrayList<>();		
+		private List<Edge> incidents = new ArrayList<>();		
 		
 		public Vertex(int vertexValue) {
 			this.vertexValue = vertexValue;
 		}
 		
 		public void addEdge(Edge edge) {
-			inzidenten.add(edge);
+			incidents.add(edge);
 		}
 		
 		public int getGrad() {
-			return inzidenten.size();
+			return incidents.size();
 		}
 		
 	}
